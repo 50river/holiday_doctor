eff99b141544bf7e7230f3e976c0698ea3abdcd3
50river <mr.50river@gmail.com>
2015-11-08 17:20:52 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+<script>
+  $.ajax({
+  url:"https://www.kimonolabs.com/api/99zbfmcw?apikey=e0cab1ea62558e9f07e4d1109a9eaf59",
+  crossDomain: true,
+  dataType: "jsonp",
+  success: function (response) {
+    //Do something with the response
+  },
+  error: function (xhr, status) {
+    //handle errors
+  }
+});</script>
