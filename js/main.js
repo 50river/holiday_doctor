8cfe0e5be07ea880c3c9b2dbbec74c9b2b634ab7
50river <mr.50river@gmail.com>
2015-11-09 13:51:44 +0900

    データ表示構造の整形

--- js/main.js
+++ js/main.js
@@ -5,12 +5,14 @@ $.ajax({
   success: function (response) {
     //Do something with the response
     var len = response.count;
+    var cat_check = response.results.collection1[0].category;
     for (var i = 0; i < len; i++) {
-      $("#test").append(response.results.collection1[i].name
-        + '<a href="http://maps.google.co.jp/maps?hl=ja&ie=UTF8&q='+response.results.collection1[i].adress+'">'+response.results.collection1[i].adress+'</a>'
-        + ' <a href="tel:'+ response.results.collection1[i].tel + '"> '+ response.results.collection1[i].tel+ '</a> '
-        + response.results.collection1[i].category + ' '
-        + response.results.collection1[i].time + '<br>');
+      $("#test").append('<ul><li class="name">' + response.results.collection1[i].name
+        + '</li><li class="category">' + response.results.collection1[i].category
+        + '</li><li class="time">'+ response.results.collection1[i].time
+        + '</li><li class="tel">' + ' <a href="tel:'+ response.results.collection1[i].tel + '"> '+ response.results.collection1[i].tel
+        + '</a></li><li class="adress"><a href="http://maps.google.co.jp/maps?hl=ja&ie=UTF8&q='+response.results.collection1[i].adress+'">'+response.results.collection1[i].adress
+        + '</a></li></ul>');
     };
   },
   error: function (xhr, status) {
