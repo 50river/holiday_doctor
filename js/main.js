0735d7ac932034c7f947e7700ffb1482141020c2
50river <mr.50river@gmail.com>
2015-11-10 18:26:31 +0900

    科別表記に変更しました。

--- js/main.js
+++ js/main.js
@@ -5,33 +5,31 @@ $.ajax({
   success: function (response) {
     //Do something with the response
     var len = response.count;
+    var j = 0;
+    var cat_check = '*****';
     for (var i = 0; i < len; i++) {
-      if (response.results.collection1[i].category == '整形外科') {
-        $(".seikeigeka").append('<ul><li class="name">' + response.results.collection1[i].name
+      if (cat_check != response.results.collection1[i].category) {
+        j++;
+        $("#accordion").append('<div class="panel panel-default">' +
+          '<div class="panel-heading">' + '<h4 class="panel-title">' +
+            '<a data-toggle="collapse" data-parent="#accordion" href="#' + response.results.collection1[i].category + '">' +
+            response.results.collection1[i].category + '</a></h4></div>' +
+            '<div id="' + response.results.collection1[i].category + '" class="panel-collapse collapse"><div class="panel-body"><div class="' + j + '"></div></div></div></div>');
+        $("."+j).append('<ul><li class="name">' + response.results.collection1[i].name
           + '</li><li class="category">' + response.results.collection1[i].category
           + '</li><li class="time">'+ response.results.collection1[i].time
           + '</li><li class="tel">' + ' <a href="tel:'+ response.results.collection1[i].tel + '"> '+ response.results.collection1[i].tel
           + '</a></li><li class="adress"><a href="http://maps.google.co.jp/maps?hl=ja&ie=UTF8&q='+response.results.collection1[i].adress+'">'+response.results.collection1[i].adress
           + '</a></li></ul>');
-      };
-      if (response.results.collection1[i].category == '小児科') {
-        $(".syounika").append('<ul><li class="name">' + response.results.collection1[i].name
-          + '</li><li class="category">' + response.results.collection1[i].category
-          + '</li><li class="time">'+ response.results.collection1[i].time
-          + '</li><li class="tel">' + ' <a href="tel:'+ response.results.collection1[i].tel + '"> '+ response.results.collection1[i].tel
-          + '</a></li><li class="adress"><a href="http://maps.google.co.jp/maps?hl=ja&ie=UTF8&q='+response.results.collection1[i].adress+'">'+response.results.collection1[i].adress
-          + '</a></li></ul>');
-      };
-      if (response.results.collection1[i].category == '内科') {
-        $(".naika").append('<ul><li class="name">' + response.results.collection1[i].name
-          + '</li><li class="category">' + response.results.collection1[i].category
-          + '</li><li class="time">'+ response.results.collection1[i].time
-          + '</li><li class="tel">' + ' <a href="tel:'+ response.results.collection1[i].tel + '"> '+ response.results.collection1[i].tel
-          + '</a></li><li class="adress"><a href="http://maps.google.co.jp/maps?hl=ja&ie=UTF8&q='+response.results.collection1[i].adress+'">'+response.results.collection1[i].adress
-          + '</a></li></ul>');
-      };
+    cat_check = response.results.collection1[i].category;
     };
-  },
+      $("."+j).append('<ul><li class="name">' + response.results.collection1[i].name
+          + '</li><li class="category">' + response.results.collection1[i].category
+          + '</li><li class="time">'+ response.results.collection1[i].time
+          + '</li><li class="tel">' + ' <a href="tel:'+ response.results.collection1[i].tel + '"> '+ response.results.collection1[i].tel
+          + '</a></li><li class="adress"><a href="http://maps.google.co.jp/maps?hl=ja&ie=UTF8&q='+response.results.collection1[i].adress+'">'+response.results.collection1[i].adress
+          + '</a></li></ul>');
+  };},
   error: function (xhr, status) {
     //handle errors
   }
