5baecff78e3302d9e9fa0d605f6724503c3b3333
50river <mr.50river@gmail.com>
2015-11-11 09:19:02 +0900

    画面サイズに合わせて、5374のように画面いっぱいに分割して表示

--- js/main.js
+++ js/main.js
@@ -5,13 +5,25 @@ $.ajax({
   success: function (response) {
     //Do something with the response
     var len = response.count;
+    var WindowHeight = $(window).height();
     var j = 0;
+    var k = 0;
     var cat_check = '*****';
+    for (var i = 0; i < len; i++) {
+      if (cat_check != response.results.collection1[i].category) {
+        k++;};
+      };
+    var accordion_height = WindowHeight / k;
+    if (window.devicePixelRatio == 2){
+    accordion_height= accordion_height*2;};
+        if(k>5){
+            if (accordion_height<100) {accordion_height=100;};
+        };
     for (var i = 0; i < len; i++) {
       if (cat_check != response.results.collection1[i].category) {
         j++;
         $("#accordion").append('<div class="panel panel-default">' +
-          '<div class="panel-heading">' + '<h4 class="panel-title">' +
+          '<div class="panel-heading"style="height:' + accordion_height + 'px" >' + '<h4 class="panel-title">' +
             '<a data-toggle="collapse" data-parent="#accordion" href="#' + response.results.collection1[i].category + '">' +
             response.results.collection1[i].category + '</a></h4></div>' +
             '<div id="' + response.results.collection1[i].category + '" class="panel-collapse collapse"><div class="panel-body"><div class="' + j + '"></div></div></div></div>');
@@ -23,7 +35,8 @@ $.ajax({
           + '</li><li class="tel">' + ' <a href="tel:'+ response.results.collection1[i].tel + '"> '+ response.results.collection1[i].tel
           + '</a></li><li class="adress"><a href="http://maps.google.co.jp/maps?hl=ja&ie=UTF8&q='+response.results.collection1[i].adress+'">'+response.results.collection1[i].adress
           + '</a></li></ul>');
-  };},
+  };
+},
   error: function (xhr, status) {
     //handle errors
   }
