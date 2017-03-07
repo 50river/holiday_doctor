e9789a5485167a7805a9afc8d992365e63c5d464
PharaohKJ <kato@phalanxware.com>
2017-03-07 22:04:41 +0900

    リファクタリング（セミコロン付けただけ）と、URLの相対パス化（いったん実験）。

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function jsonLoaded(response){
     var list_text = '';
     var j = 1;
     for (var i = 0; i < len; i++) {
-        var collection = response.results.collection1[i]
+        var collection = response.results.collection1[i];
         var category = cat[i];
         if (cat_check != category) {
 
@@ -53,8 +53,8 @@ function jsonLoaded(response){
     accordion_element.append(accordion_text);
 
     $(".panel").on("shown.bs.collapse", function() {
-        scrollToElement(this)
-    })
+        scrollToElement(this);
+    });
 
 }
 function createInsertElement(accordion_height, category, category_index, list_text) {
@@ -67,20 +67,17 @@ function createInsertElement(accordion_height, category, category_index, list_te
 }
 
 $.ajax({
-//  url:"https://www.kimonolabs.com/api/99zbfmcw?apikey=e0cab1ea62558e9f07e4d1109a9eaf59&kimmodify=1",
-  url:"https://pharaohkj.github.io/holiday_doctor/data.json",
+  url: "/holiday_doctor/data.json",
   crossDomain: true,
   dataType: "json",
   success: function (response) {
-    jsonLoaded(response)
+      jsonLoaded(response);
   },
   error: function (xhr, status) {
-    alert(status)
-//    local test
-    var json={"name":"holiday_doctor","count":8,"frequency":"Daily","version":6,"newdata":true,"lastrunstatus":"success","thisversionstatus":"success","nextrun":"Thu Nov 12 2015 07:50:52 GMT+0000 (UTC)","thisversionrun":"Wed Nov 11 2015 07:50:51 GMT+0000 (UTC)","results":{"collection1":[{"name":"斉藤小児科医院","address":"白山市馬場二丁目16番地","tel":"076-275-3110","category":"小児科","time":"9:00～12:00","index":4,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?a=3"},{"name":"医療法人社団大倉外科医院","address":"野々市市本町3丁目7-12","tel":"076-248-3263","category":"外科","time":"9:00～12:00","index":1,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?a=3"},{"name":"医療法人社団　長尾医院","address":"白山市美川永代町ソ248","tel":"076-278-2156","category":"内科 \n外科","time":"9:00～12:00","index":6,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?p=2&a=3"},{"name":"織田内科クリニック","address":"白山市鶴来本町3丁目ヲ11","tel":"076-273-9100","category":"内科","time":"9:00～12:00","index":2,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?a=3"},{"name":"喜多内科医院","address":"野々市市横宮町7-20","tel":"076-248-0020","category":"内科","time":"9:00～12:00","index":3,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?a=3"},{"name":"てらしま内科クリニック","address":"白山市中奥町91-3","tel":"076-274-8666","category":"内科","time":"9:00～12:00","index":5,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?a=3"}],"collection2":[{"date":"11月15日 （日）","index":7,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?a=3"},{"date":"11月15日 （日）","index":8,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?p=2&a=3"}]}}
-    jsonLoaded(json)
+      alert(status);
+      //    local test
+      var json={"name":"holiday_doctor","count":8,"frequency":"Daily","version":6,"newdata":true,"lastrunstatus":"success","thisversionstatus":"success","nextrun":"Thu Nov 12 2015 07:50:52 GMT+0000 (UTC)","thisversionrun":"Wed Nov 11 2015 07:50:51 GMT+0000 (UTC)","results":{"collection1":[{"name":"斉藤小児科医院","address":"白山市馬場二丁目16番地","tel":"076-275-3110","category":"小児科","time":"9:00～12:00","index":4,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?a=3"},{"name":"医療法人社団大倉外科医院","address":"野々市市本町3丁目7-12","tel":"076-248-3263","category":"外科","time":"9:00～12:00","index":1,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?a=3"},{"name":"医療法人社団　長尾医院","address":"白山市美川永代町ソ248","tel":"076-278-2156","category":"内科 \n外科","time":"9:00～12:00","index":6,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?p=2&a=3"},{"name":"織田内科クリニック","address":"白山市鶴来本町3丁目ヲ11","tel":"076-273-9100","category":"内科","time":"9:00～12:00","index":2,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?a=3"},{"name":"喜多内科医院","address":"野々市市横宮町7-20","tel":"076-248-0020","category":"内科","time":"9:00～12:00","index":3,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?a=3"},{"name":"てらしま内科クリニック","address":"白山市中奥町91-3","tel":"076-274-8666","category":"内科","time":"9:00～12:00","index":5,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?a=3"}],"collection2":[{"date":"11月15日 （日）","index":7,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?a=3"},{"date":"11月15日 （日）","index":8,"url":"http://i-search.pref.ishikawa.jp/toban/index.php?p=2&a=3"}]}};
+      jsonLoaded(json);
 
   }
 });
-
-
